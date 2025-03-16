857cd5cfe8b8121feb67987c22535cb494afaaff
MrOrangeade <tayyabdev@outlook.com>
2025-03-16 17:37:17 +0000

    removed user_id as its not needed right now, and created a task factory to help with writing tests

--- database/migrations/2025_03_16_130134_create_tasks_table.php
+++ database/migrations/2025_03_16_130134_create_tasks_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
 
             $table->string('task');
             $table->boolean('completed')->default(false);
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->timestamps();
         });
