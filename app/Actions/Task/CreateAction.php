653ae0a7d3d9cbad76b2f9c7e03b8411539da41d
MrOrangeade <tayyabdev@outlook.com>
2025-03-16 17:38:00 +0000

    cleanup action and include vite resource in blade

--- app/Actions/Task/CreateAction.php
+++ app/Actions/Task/CreateAction.php
@@ -7,8 +7,9 @@ use App\Models\Task;
 
 class CreateAction {
 
+
     public function execute(TaskDTO $taskDTO): Task {
-        return Task::create(array_merge($taskDTO->toArray(), ['user_id' => auth()->id()]));
+        return Task::create($taskDTO->toArray());
     }
 
 }
