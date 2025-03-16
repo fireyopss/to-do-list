5148114aba8fe9532ca3ed324d71c44b03d2aa1a
MrOrangeade <tayyabdev@outlook.com>
2025-03-16 18:03:51 +0000

    default layout

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MLP To-Do</title>
+@extends('layouts.default')
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@section('title')
+    Tasks
+@endsection
 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+@section('content')
+<div class="m-12">
+<p class="text-5xl">tasks</p>
+</div>
 
-    @vite('resources/css/app.css')
-</head>
-<body class="bg-black text-white text-5xl flex items-center justify-center h-screen">
-    <p class="text-5xl">tasks</p>
-</body>
-</html>
+@endsection
